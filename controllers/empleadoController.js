1b7558e9fcd0be730181c42ab8164241433f5e05
Santiago Marín <esmarina@unal.edu.co>
2020-09-26 20:16:53 -0500

    Controlador para buscar un empleado por su id creado

--- controllers/empleadoController.js
+++ controllers/empleadoController.js
@@ -4,12 +4,30 @@ exports.empleadosGetAll = (req, res) => {
   Empleado.find()
   .then((emps) => {
     res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(emps);
+    res.status(200).json({ success: true, data: emps });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log(err);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(401).json({ success: false, data: err });
+  });
 }
 
-exports.empleadosCreatePost = (req, res, next) => {
+exports.empleadosGetOne = (req, res) => {
+  var id = req.params.id;
+  Empleado.findOne({ id })
+  .then((emp) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json({ success: true, data: emp });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(401).json({ success: false, data: err });
+  });
+}
+
+exports.empleadosCreatePost = (req, res) => {
   // Se desglosa la petición de esta manera para facilitar la lectura y entendimiento del código
   var id = req.body.id;
   var name = req.body.name;
@@ -26,7 +44,11 @@ exports.empleadosCreatePost = (req, res, next) => {
   .then((emp) => {
     res.setHeader('Content-Type', 'application/json');
     // Respondemos con el empleado creado
-    res.status(200).json(emp);
+    res.status(200).json({ success: true, data: emp });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => {
+    console.log(err);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(401).json({ success: false, data: err });
+  });
 }
