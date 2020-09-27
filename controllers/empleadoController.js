e5c6438b321fa92920b74dfbbe4c16477fde064e
Santiago Marín <esmarina@unal.edu.co>
2020-09-26 20:45:54 -0500

    Controlador para eliminar un empleado creado

--- controllers/empleadoController.js
+++ controllers/empleadoController.js
@@ -4,12 +4,12 @@ exports.empleadosGetAll = (req, res) => {
   Empleado.find()
   .then((emps) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json({ success: true, data: emps });
+    res.status(200).json({ success: true, data: emps, msg: "Empleados encontrados exitosamente!" });
   })
   .catch((err) => {
     console.log(err);
     res.setHeader('Content-Type', 'application/json');
-    res.status(401).json({ success: false, data: err });
+    res.status(403).json({ success: false, data: null, msg: "Ha ocurrido un error", error: err.errors });
   });
 }
 
@@ -18,12 +18,25 @@ exports.empleadosGetOne = (req, res) => {
   Empleado.findOne({ id })
   .then((emp) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json({ success: true, data: emp });
+    if (emp) {
+      if (emp.contractTypeName == "HourlySalaryEmployee") {
+        emp.salarioAnual = 120 * emp.hourlySalary * 12;
+
+        // Se pone 'else' directamente porque se tiene la restricción en el esquema del empleado de 
+        // que un tipo de contrato TIENE que ser igual a "HourlySalaryEmployee" o a "MonthlySalaryEmployee"
+      } else {  
+        emp.salarioAnual = emp.monthlySalary * 12;
+      }
+      res.status(200).json({ success: true, data: emp, msg: "Usuario encontrado exitosamente!" });
+
+    } else {
+      res.status(200).json({ success: false, data: null, msg: "Usuario no encontrado" });
+    }
   })
   .catch((err) => {
     console.log(err);
     res.setHeader('Content-Type', 'application/json');
-    res.status(401).json({ success: false, data: err });
+    res.status(403).json({ success: false, data: null, msg: "Ha ocurrido un error", error: err.errors });
   });
 }
 
@@ -36,7 +49,7 @@ exports.empleadosCreatePost = (req, res) => {
   var roleName = req.body.roleName;
   var roleDescription = req.body.roleDescription;
   var hourlySalary = req.body.hourlySalary;
-  var monthlySalary = req.body.hourlySalary;
+  var monthlySalary = req.body.monthlySalary;
 
   var empleadoNuevo = new Empleado({ id, name, contractTypeName, roleId, roleName, roleDescription,
                                     hourlySalary, monthlySalary});
@@ -44,11 +57,25 @@ exports.empleadosCreatePost = (req, res) => {
   .then((emp) => {
     res.setHeader('Content-Type', 'application/json');
     // Respondemos con el empleado creado
-    res.status(200).json({ success: true, data: emp });
+    res.status(200).json({ success: true, data: emp, msg: "Empleado creado exitosamente!" });
   })
   .catch((err) => {
     console.log(err);
     res.setHeader('Content-Type', 'application/json');
-    res.status(401).json({ success: false, data: err });
+    res.status(403).json({ success: false, data: null, msg: "Ha ocurrido un error", error: err.errors });
+  });
+}
+
+exports.empleadosDeleteOne = (req, res) => {
+  var id = req.params.id;
+  Empleado.findOneAndDelete({ id })
+  .then((emp) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json({ success: true, data: emp, msg: "Empleado eliminado exitosamente!" });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.setHeader('Content-Type', 'application/json');
+    res.status(403).json({ success: false, data: null, msg: "Ha ocurrido un error", error: err.errors });
   });
 }
