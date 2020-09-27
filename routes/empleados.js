a21e8b3343766dcb06a921169bef9a51237cb49e
Santiago Marín <esmarina@unal.edu.co>
2020-09-26 20:18:43 -0500

    ruta para obtener un empleado por su id agregada

--- routes/empleados.js
+++ routes/empleados.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const empleadoController = require("../controllers/empleadoController");
 
+// Se separan las rutas de los controladores para facilitar el escalamiento de la API
 router.get("/", empleadoController.empleadosGetAll);
+router.get("/:id", empleadoController.empleadosGetOne);
 router.post("/", empleadoController.empleadosCreatePost);
 
 module.exports = router;
