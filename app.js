3e28bbcf96383bc78a9e3bd14fdbb6cd5649561c
Santiago Marín <esmarina@unal.edu.co>
2020-09-26 19:00:46 -0500

    Ruta para obtener todos los empleados creadas

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var empleadosRouter = require('./routes/empleados');
 
 // Conexión a la BD
 require("./config/db-connection");
@@ -16,6 +18,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());  // Permite peticiones de todos los dominios
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/empleados', empleadosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/empleados.js
+++ routes/empleados.js
@@ -2,4 +2,7 @@ const express = require('express');
 const router = express.Router();
 const empleadoController = require("../controllers/empleadoController");
 
+router.get("/", empleadoController.empleadosGetAll);
 router.post("/", empleadoController.empleadosCreatePost);
+
+module.exports = router;
